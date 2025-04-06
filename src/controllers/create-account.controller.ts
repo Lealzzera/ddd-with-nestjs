8ef137bbddc24cc203a5b4790df0e2ee6f3c5ef6
Matheus Leal <matheus.mleall@gmail.com>
2025-04-06 17:24:43 -0300

    initial project commit

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,47 @@
+import { ConflictException, UsePipes } from "@nestjs/common";
+import { Body, Controller, HttpCode, Post } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { hash } from "bcrypt";
+import z from "zod";
+import { create } from "domain";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+
+@Controller("/accounts")
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(
+    @Body()
+    { name, email, password }: CreateAccountBodySchema
+  ) {
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithSameEmail)
+      throw new ConflictException("Email provided already exists");
+
+    const hashedPassowrd = await hash(password, 8);
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassowrd,
+      },
+    });
+  }
+}
