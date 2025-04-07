7323984442b66624b7bbe753f2fc0ab4d15f97ca
Matheus Leal <matheus.mleall@gmail.com>
2025-04-07 00:01:14 -0300

    setted up unit and e2e tests with vitest and swc

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,23 @@
+import { resolve } from "path";
+import swc from "unplugin-swc";
+import { defineConfig } from "vitest/config";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: "./",
+    include: ["**/*.e2e-spec.ts"],
+  },
+  plugins: [
+    swc.vite({
+      module: { type: "es6" },
+    }),
+    tsConfigPaths(),
+  ],
+  resolve: {
+    alias: {
+      src: resolve(__dirname, "./src"),
+    },
+  },
+});
