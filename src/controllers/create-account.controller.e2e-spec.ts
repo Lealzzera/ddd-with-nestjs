2665f58f46664588ca8f05eab3e8a21e20b8674f
Matheus Leal <matheus.mleall@gmail.com>
2025-04-07 00:42:21 -0300

    created create-account controller e2e tests

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,3 +1,38 @@
-test("", () => {
-  expect(1 + 1).toBe(2);
+import { AppModule } from "@/app.module";
+import { PrismaService } from "@/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+
+describe("Create acount (E2E)", () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+
+    await app.init();
+  });
+
+  test("[POST] /accounts", async () => {
+    const response = await request(app.getHttpServer()).post("/accounts").send({
+      name: "John Doe",
+      email: "johndoe@acme.com",
+      password: "1234567890",
+    });
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: "johndoe@acme.com",
+      },
+    });
+
+    expect(userOnDatabase).toBeTruthy();
+    expect(response.statusCode).toBe(201);
+  });
 });
