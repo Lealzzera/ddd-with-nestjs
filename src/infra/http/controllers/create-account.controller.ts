0223af431828c67c0b116dc22019985b0e9597ac
Matheus Leal <matheus.mleall@gmail.com>
2025-04-07 11:50:51 -0300

    implemented all of repositories and changed the structure to import prisma service inside of database module

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -1,9 +1,9 @@
 import { ConflictException, UsePipes } from "@nestjs/common";
 import { Body, Controller, HttpCode, Post } from "@nestjs/common";
-import { PrismaService } from "@/infra/prisma/prisma.service";
 import { hash } from "bcrypt";
 import z from "zod";
 import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
 
 const createAccountBodySchema = z.object({
   name: z.string(),
