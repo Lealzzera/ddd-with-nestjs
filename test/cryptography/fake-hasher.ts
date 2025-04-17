68afbeb48e9e76baf39557cc88ddfe1dc9330b9f
Matheus Leal <matheus.mleall@gmail.com>
2025-04-17 12:47:20 -0300

    Created cryptography stubs

---
+++ test/cryptography/fake-hasher.ts
@@ -0,0 +1,11 @@
+import { HashComparer } from "@/domain/forum/application/cryptography/hash-comparer";
+import { HasheGenerator } from "@/domain/forum/application/cryptography/hash-generator";
+
+export class FakeHasher implements HasheGenerator, HashComparer {
+  async hash(plain: string): Promise<string> {
+    return plain.concat("-hashed");
+  }
+  async compare(plain: string, hash: string): Promise<boolean> {
+    return plain.concat("-hashed") === hash;
+  }
+}
