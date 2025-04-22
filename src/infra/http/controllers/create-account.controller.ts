cd7362b826e216529b052537f580f8980391d772
Matheus Leal <matheus.mleall@gmail.com>
2025-04-22 07:23:16 -0300

    refactored authenticate, create account controller, now its not using prisma directly, its using the use case and handling with errors

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -1,9 +1,13 @@
-import { ConflictException, UsePipes } from "@nestjs/common";
+import {
+  BadRequestException,
+  ConflictException,
+  UsePipes,
+} from "@nestjs/common";
 import { Body, Controller, HttpCode, Post } from "@nestjs/common";
-import { hash } from "bcrypt";
 import z from "zod";
 import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
-import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { RegisterStudentUseCase } from "@/domain/forum/application/use-cases/register-student";
+import { StudentAlreadyExistsError } from "@/domain/forum/application/use-cases/errors/student-already-exists-error";
 
 const createAccountBodySchema = z.object({
   name: z.string(),
@@ -15,7 +19,7 @@ type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
 
 @Controller("/accounts")
 export class CreateAccountController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private registerStudent: RegisterStudentUseCase) {}
 
   @Post()
   @HttpCode(201)
@@ -23,5 +27,21 @@ export class CreateAccountController {
   async handle(
     @Body()
     { name, email, password }: CreateAccountBodySchema
-  ) {}
+  ) {
+    const result = await this.registerStudent.execute({
+      name,
+      email,
+      password,
+    });
+
+    if (result.isLeft()) {
+      const error = result.value;
+      switch (error.constructor) {
+        case StudentAlreadyExistsError:
+          throw new ConflictException(error.message);
+        default:
+          throw new BadRequestException(error.message);
+      }
+    }
+  }
 }
